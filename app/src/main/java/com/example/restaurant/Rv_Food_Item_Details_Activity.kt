d068dece33b334ed91c14c7dd4ffd0209380c48c
tawhid12 <tawhid99@gmail.com>
2019-09-01 18:07:55 +0600

    Homework App

---
+++ app/src/main/java/com/example/restaurant/Rv_Food_Item_Details_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.restaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Rv_Food_Item_Details_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rv__food__item__details_)
+    }
+}
